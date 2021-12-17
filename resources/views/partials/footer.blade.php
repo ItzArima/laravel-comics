2a9611938d2afe24187aa31293d74f8b9c67c158
ItzArima <skaypaprilia@gmail.com>
2021-12-17 18:34:13 +0100

    header and footer added

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,78 @@
 <footer>
-    <h1>this is the footer</h1>
+    <div class="footer-main">
+        <div class="content-centered">
+            <div class="footer-content">
+                <div class="aside-container">    
+                    <aside>
+                        <div class="dc-comics">
+                            <h2>DC COMICS</h2>
+                            <ul>
+                                <li><a href="#">Characters</a></li>
+                                <li><a href="#">Comics</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">TV</a></li>
+                                <li><a href="#">Games</a></li>
+                                <li><a href="#">Videos</a></li>
+                                <li><a href="#">News</a></li>
+                            </ul>
+                        </div>
+                        <div class="shop">
+                            <h2>SHOP</h2>
+                            <ul>
+                                <li><a href="#">Shop DC</a></li>
+                                <li><a href="#">Shop DC Collectibles</a></li>
+                            </ul>
+                        </div>
+                    </aside>
+                    <aside>
+                        <div class="dc">
+                            <h2>DC</h2>
+                            <ul>
+                                <li><a href="#">Terms Of Use</a></li>
+                                <li><a href="#">Privacy policy(New)</a></li>
+                                <li><a href="#">Ad Choices</a></li>
+                                <li><a href="#">Advertising</a></li>
+                                <li><a href="#">Jobs</a></li>
+                                <li><a href="#">Subscriptions</a></li>
+                                <li><a href="#">Talent Workshops</a></li>
+                                <li><a href="#">CPSC Certificates</a></li>
+                                <li><a href="#">Ratings</a></li>
+                                <li><a href="#">Shop Help</a></li>
+                                <li><a href="#">Contact Us</a></li>
+                            </ul>
+                        </div>
+                    </aside>
+                    <aside>
+                        <div class="sites">
+                            <h2>SITES</h2>
+                            <ul>
+                                <li><a href="#">DC</a></li>
+                                <li><a href="#">MAD Magazine</a></li>
+                                <li><a href="#">DC Kids</a></li>
+                                <li><a href="#">DC Universe</a></li>
+                                <li><a href="#">DC Power Vista</a></li>
+                            </ul>
+                        </div>
+                    </aside>
+                </div> 
+                <div class="copyright-container">
+                    <p>All Site Content TM and &copy; 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>. All rights reserved</p>
+                    <a href="#">Cookies Settings</a>
+                </div>     
+            </div>
+            <div class="footer-logo">
+            </div>
+        </div>    
+    </div>
+    <div class="sing-up-container">
+        <button>SING UP NOW!</button>
+        <div class="follow-us-container">
+            <h2>FOLLOW US</h2>
+            <a href="#"><img src="../img/footer-facebook.png" alt="" srcset=""></a>
+            <a href="#"><img src="../img/footer-twitter.png" alt="" srcset=""></a>
+            <a href="#"><img src="../img/footer-youtube.png" alt="" srcset=""></a>
+            <a href="#"><img src="../img/footer-pinterest.png" alt="" srcset=""></a>
+            <a href="#"><img src="../img/footer-periscope.png" alt="" srcset=""></a>
+        </div>
+    </div>
 </footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,36 @@
 <header>
-    <h1>this is the header</h1>
+    <div class="header-top-bar">
+        <p>DC POWER &#8480; VISA &REG;</p>
+        <select name="sites" id="sites">
+            <option value="">ADDITIONAL DC SITES</option>
+            <option value="option1">Option1</option>
+            <option value="option2">Option2</option>
+        </select>
+    </div>
+    <div class="header-main">
+        <div class="logo-container">
+            <img src="../img/dc-logo.png" alt="" srcset="">
+        </div>
+        <nav>
+            <a href="{{route('characters')}}">CHARACTERS</a>
+            <a href="{{route('comics')}}">COMICS</a>
+            <a href="{{route('movies')}}">MOVIES</a>
+            <a href="{{route('tv')}}">TV</a>
+            <a href="{{route('games')}}">GAMES</a>
+            <a href="{{route('collectibles')}}">COLLECTIBLES</a>
+            <a href="{{route('videos')}}">VIDEOS</a>
+            <a href="{{route('fans')}}">FANS</a>
+            <a href="{{route('news')}}">NEWS</a>    
+            <div class="shop-select">
+                <a href="{{route('shop')}}">SHOP</a>
+                <p>&dtrif;</p>
+            </div>        
+        </nav>
+        <div class="search-container">
+            <form action="#">
+                <input type="text" placeholder="Search">
+                <img src="../img/search-solid.svg" alt="">
+            </form>
+        </div>
+    </div>
 </header>
